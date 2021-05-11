673146bec8821014bf514527c4a67daa5f1b04a7
aljica <aljica@kth.se>
2021-05-11 16:58:22 +0200

    setup basic fetch with params

--- src/routes/CountrySearchResults.js
+++ src/routes/CountrySearchResults.js
@@ -3,17 +3,32 @@ import { Component } from 'react';
 class CountrySearchResults extends Component {
   constructor(props) {
     super(props);
-    const URL = 'api.geonames.org/search' + this.props.location.search;
-    console.log(URL);
+    // const URL = 'api.geonames.org/search?q=' + this.props.location.search;
+    const URL = 'http://api.geonames.org/searchJSON?q=' + this.props.location.search.replace('?', '') + '&maxRows=3&orderby=population&username=weknowit';
     this.state = {
       loading: true,
+      url: null,
     };
+    this.url = URL;
+  }
+
+  parseData(data) {
+    console.log(data);
+  }
+
+  getCities(URL) {
+    console.log(URL);
+    fetch(URL)
+      .then((response) => response.json())
+      .then((data) => this.parseData(data))
+      .catch((e) => console.log(e));
   }
 
   render() {
     return (
       <div>
         Hello World
+        {this.getCities(this.url)}
       </div>
     );
   }
