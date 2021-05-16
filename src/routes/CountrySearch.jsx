1be0a4cd93272df79347dcd579b4b8bf72086707
aljica <aljica@kth.se>
2021-05-16 13:45:45 +0200

    refactor CountrySearch and separate input into new component

--- src/routes/CountrySearch.jsx
+++ src/routes/CountrySearch.jsx
@@ -1,50 +1,42 @@
 import { Typography, Grid, Button, Input, Container } from '@material-ui/core';
-import { Component } from 'react';
+import { useState } from 'react';
 import magnifyingGlass from '../static/images/magnifying-glass.png';
+import UserInput from '../components/UserInput';
 
-class CountrySearch extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      inputValue: '',
-    };
-  }
+const CountrySearch = (props) => {
+  const [inputValue, setInputValue] = useState('');
 
-  redirect(path) {
-    this.props.history.push({
+  function redirect(path) {
+    props.history.push({
       pathname: path,
-      search: this.state.inputValue,
+      search: inputValue,
     });
   }
 
-  updateInputValue(e) {
-    this.setState({
-      inputValue: e.target.value,
-    });
+  function updateInputValue(e) {
+    setInputValue(e.target.value);
   }
 
-  render() {
-    return (
-      <div>
-        <Container maxWidth="sm">
-          <Grid container justify="center">
-            <Typography variant="h6">
-              SEARCH BY COUNTRY
-            </Typography>
-          </Grid>
-          <br></br>
-          <Grid container justify="center">
-            <Input value={this.state.inputValue} onChange={(e) => this.updateInputValue(e)} fullWidth placeholder="Enter a country" inputProps={{style: { textAlign: "center" }}} />
-          </Grid>
-          <Grid container justify="center">
-            <Button disabled={this.state.inputValue === ''} onClick={() => this.redirect('/country/results')}>
-                <img src={magnifyingGlass} alt="Search" width="50" height="50"></img>
-              </Button>
-          </Grid>
-        </Container>
-      </div>
-    );
-  }
+  return (
+    <div>
+      <Container maxWidth="sm">
+        <Grid container justify="center">
+          <Typography variant="h6">
+            SEARCH BY COUNTRY
+          </Typography>
+        </Grid>
+        <br></br>
+        <Grid container justify="center">
+          <UserInput inputValue={inputValue} updateInputValue={updateInputValue} placeholder={"Enter a country"} />
+        </Grid>
+        <Grid container justify="center">
+          <Button disabled={inputValue === ''} onClick={() => redirect('/country/results')}>
+            <img src={magnifyingGlass} alt="Search" width="50" height="50"></img>
+          </Button>
+        </Grid>
+      </Container>
+    </div>
+  );
 }
 
 export default CountrySearch;
