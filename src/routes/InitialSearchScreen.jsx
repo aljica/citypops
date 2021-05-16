5966ce6b727fe6ae025dd1f3bc7582221cbd4c5c
aljica <aljica@kth.se>
2021-05-16 13:47:57 +0200

    refactor initialsearchscreen

--- src/routes/InitialSearchScreen.jsx
+++ src/routes/InitialSearchScreen.jsx
@@ -1,35 +1,25 @@
-import { Component } from 'react';
 import { Grid, Button, Container } from '@material-ui/core';
 
-class InitialSearchScreen extends Component {
-  constructor(props) {
-    super(props);
+const InitialSearchScreen = (props) => {
+
+  function redirect(path) {
+    props.history.push(path);
   }
 
-  componentDidMount() {
-    console.log('mounted');
-  }
-
-  redirect(path) {
-    this.props.history.push(path);
-  }
-
-  render() {
-    return (
-      <div>
-        <Container maxWidth="sm">
-          <Grid container spacing={1} justify="center">
-            <Grid item xs={6} align="center">
-              <Button onClick={() => this.redirect('/city')} size="large" variant="contained">SEARCH BY CITY</Button>
-            </Grid>
-            <Grid item xs={6} align="center">
-              <Button onClick={() => this.redirect('/country')} size="large" variant="contained">SEARCH BY COUNTRY</Button>
-            </Grid>
+  return (
+    <div>
+      <Container maxWidth="sm">
+        <Grid container spacing={1} justify="center">
+          <Grid item xs={6} align="center">
+            <Button onClick={() => redirect('/city')} size="large" variant="contained">SEARCH BY CITY</Button>
           </Grid>
-        </Container>
-      </div>
-    );
-  }
+          <Grid item xs={6} align="center">
+            <Button onClick={() => redirect('/country')} size="large" variant="contained">SEARCH BY COUNTRY</Button>
+          </Grid>
+        </Grid>
+      </Container>
+    </div>
+  );
 }
 
 export default InitialSearchScreen;
