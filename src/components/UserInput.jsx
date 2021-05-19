0885d0b4877cf95902d2765ea653648199753c33
aljica <aljica@kth.se>
2021-05-19 15:13:28 +0200

    enable submitting form by pressing enter

--- src/components/UserInput.jsx
+++ src/components/UserInput.jsx
@@ -2,7 +2,7 @@ import { Input } from '@material-ui/core';
 
 const UserInput = (props) => {
   return (
-    <Input value={props.inputValue} onChange={(e) => props.updateInputValue(e)} fullWidth placeholder={props.placeholder} inputProps={{style: { textAlign: "center" }}} />
+    <Input value={props.inputValue} onKeyDown={(e) => props.handleKeyboardClick(e)} onChange={(e) => props.updateInputValue(e)} fullWidth placeholder={props.placeholder} inputProps={{style: { textAlign: "center" }}} />
   );
 }
 
--- src/routes/CitySearch.jsx
+++ src/routes/CitySearch.jsx
@@ -15,12 +15,18 @@ const CitySearch = (props) => {
 
   function updateSoughtCity(e) {
     // User may only input letters
-    const lettersRegEx = /^$|^[a-zA-Z\s]+$/;
+    const lettersRegEx = /^$|^[a-zA-z\s]+$/;
     if (!(lettersRegEx.test(e.target.value))) return;
     if (soughtCity.length > 20) {
       if (!(e.target.value.length < soughtCity.length)) return;
     }
     setSoughtCity(e.target.value);
+  }
+
+  function handleKeyboardClick(e) {
+    if (e.key === 'Enter') {
+      if (soughtCity.length > 0) redirect('/city/results', soughtCity);
+    }
   }
 
   return (
@@ -33,7 +39,7 @@ const CitySearch = (props) => {
           </Grid>
           <br></br>
           <Grid container justify="center">
-            <UserInput inputValue={soughtCity} updateInputValue={updateSoughtCity} placeholder='Enter a city' />
+            <UserInput inputValue={soughtCity} handleKeyboardClick={handleKeyboardClick} updateInputValue={updateSoughtCity} placeholder='Enter a city' />
           </Grid>
           <Grid container justify="center">
             <SearchButton inputValue={soughtCity} redirect={redirect} path='/city/results' />
